fe4433b939ac50043c51291f2cf0f56bc4ff5837
Malki Heulan <malkiheulan@gmail.com>
2024-05-14 09:30:30 +0200

    Aéroport final ( à review)

--- src/modele/Aeroport.java
+++ src/modele/Aeroport.java
@@ -1,26 +1,82 @@
 package modele;
+/**
+ * Classe représentant un aéroport
+ */
 public class Aeroport {
+    /**
+     * Nom de l'aéroport
+     */
     private String nom;
+    /**
+     * Adresse de l'aéroport
+     */
     private String adresse;
     
-    public Aeroport(String nom, String adresse){
-
+    /**
+     * Constructeur de la classe Aeroport
+     * @param nom le nom de l'aéroport
+     * @param adresse l'adresse de l'aéroport
+     */
+    public Aeroport(String nom, String adresse) throws IllegalArgumentException{
+        if (nom == null || adresse == null){
+            throw new IllegalArgumentException("Le nom ou l'adresse de l'aéroport est null");
+        } else {
+            this.nom = nom;
+            this.adresse = adresse;
+        }
     }
+    /**
+     * Getter du nom de l'aéroport
+     * @return le nom de l'aéroport
+     */
     public String getNom(){
         return this.nom;
     }
+
+    /**
+     * Getter de l'adresse de l'aéroport
+     * @return l'adresse de l'aéroport
+     */
     public String getAdresse(){
         return this.adresse;
     }
-    public void setNom(String nom){
-        
-    
+    /**
+     * Setter du nom de l'aéroport
+     * @param nom le nom de l'aéroport
+     */
+    public void setNom(String nom) throws IllegalArgumentException{
+        if (nom == null){
+            throw new IllegalArgumentException("Le nom de l'aéroport est null");
+        } else {
+            this.nom = nom;
+        }
     }
-    public void setAdresse(String adresse){
-
+    /**
+     * Setter de l'adresse de l'aéroport
+     * @param adresse l'adresse de l'aéroport
+     */
+    public void setAdresse(String adresse) throws IllegalArgumentException{
+        if (adresse == null){
+            throw new IllegalArgumentException("L'adresse de l'aéroport est null");
+        } else {
+            this.adresse = adresse;
+        }
     }
-    public boolean isSameAero(Aeroport autreAero){
+    /**
+     * Méthode permettant de comparer deux aéroports
+     * @param autreAero l'aéroport à comparer
+     * @return true si les deux aéroports sont les mêmes, false sinon
+     */
+    public boolean isSameAero(Aeroport autreAero) throws IllegalArgumentException{
         boolean isSame = false;
+        if (autreAero == null){
+            throw new IllegalArgumentException("L'aéroport à comparer est null");
+        } else {
+            if (this.nom.equalsIgnoreCase(autreAero.getNom())) {
+                isSame = true;
+            }
+        }
+
         return isSame;
     }
 }
