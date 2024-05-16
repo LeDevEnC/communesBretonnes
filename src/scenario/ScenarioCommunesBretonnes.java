85f486e6292f74bd3227a269f8f74d76069c0854
Malki Heulan <malkiheulan@gmail.com>
2024-05-16 09:46:32 +0200

    Creation du package scenario

--- src/scenario/ScenarioCommunesBretonnes.java
+++ src/scenario/ScenarioCommunesBretonnes.java
@@ -1,14 +1,18 @@
+package scenario;
 import model.dao.AnneeDAO;
 import model.data.*;
 
 public class ScenarioCommunesBretonnes {
     public static void main(String[] args){
+
         AnneeDAO anneeDAO = new AnneeDAO();
-        System.out.println(anneeDAO.findByID(2019L).getAnnee() + " : " + anneeDAO.findByID(2019L).getTauxInflation());
+        Annee annee1 = new Annee(2050, 40);
+        anneeDAO.delete(annee1);
 
         System.out.println("Liste des années : ");
         for (Annee annee : anneeDAO.findAll()) {
             System.out.println(annee.getAnnee() + " : " + annee.getTauxInflation());
         }
+
     }
 }
