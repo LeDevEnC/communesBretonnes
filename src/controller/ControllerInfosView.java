d23b08308c60e9c2bee64825e62b57baf1499ed8
Malki <malkiheulan@gmail.com>
2024-06-17 10:43:27 +0200

    ajout gestion des couleur selon le score

--- src/controller/ControllerInfosView.java
+++ src/controller/ControllerInfosView.java
@@ -96,25 +96,8 @@ public class ControllerInfosView extends Controller {
                 score = this.lineClicked.calculateScoreSurfaceMoy();
                 break;
         }
-        Color color = getColorForScore(score);
+        Color color = lineClicked.getColorForScore(score);
         return String.format("%s", color.toString(), value);
-    }
-
-
-    private Color getColorForScore(double score) {
-        if (score >= 90) {
-            return Color.rgb(0, 176, 80); // Vert foncé pour les scores très élevés
-        } else if (score >= 80) {
-            return Color.rgb(146, 208, 80); // Vert clair pour les scores élevés
-        } else if (score >= 70) {
-            return Color.rgb(255, 255, 0); // Jaune pour les scores moyens élevés
-        } else if (score >= 60) {
-            return Color.rgb(255, 192, 0); // Orange pour les scores moyens
-        } else if (score >= 50) {
-            return Color.rgb(255, 152, 0); // Orange foncé pour les scores moyens bas
-        } else {
-            return Color.rgb(255, 0, 0); // Rouge pour les scores bas
-        }
     }
 
 
